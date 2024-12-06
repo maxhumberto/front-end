060a738dd14683d0f6cc23c97f451e3b5d95342a
maxhumberto@gmail.com <maxhumberto@gmail.com>
2024-12-05 22:47:49 -0300

    terminei projeto desafiodetona ralph e dei upgrade na vida

---
+++ desafiodio/detonaRalph/src/scripts/engine.js
@@ -0,0 +1,85 @@
+const state = {
+    view: {
+      squares: document.querySelectorAll(".square"),
+      enemy: document.querySelector(".enemy"),
+      timeLeft: document.querySelector("#time-left"),
+      score: document.querySelector("#score"),
+      lives: document.querySelector(".menu-lives h2"), 
+    },
+    values: {
+      gameVelocity: 1000,
+      hitPosition: 0,
+      result: 0,
+      curretTime: 60,
+      lives: 3,
+    },
+    actions: {
+      timerId: setInterval(randomSquare, 1000),
+      countDownTimerId: setInterval(countDown, 1000),
+    },
+  };
+  
+  function loseLife() {
+    state.values.lives--; // Diminui uma vida
+    state.view.lives.textContent = `x${state.values.lives}`; // Atualiza na interface
+
+    if (state.values.lives <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! Você perdeu todas as vidas.");
+    } else {
+        playSound("error"); // Toca som de erro ao perder vida
+    }
+}
+
+
+
+
+  function countDown() {
+    state.values.curretTime--;
+    state.view.timeLeft.textContent = state.values.curretTime;
+  
+    if (state.values.curretTime <= 0) {
+      clearInterval(state.actions.countDownTimerId);
+      clearInterval(state.actions.timerId);
+      alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+  }
+  
+  function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+  }
+  
+  function randomSquare() {
+    state.view.squares.forEach((square) => {
+      square.classList.remove("enemy");
+    });
+  
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+  }
+  
+  function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+      square.addEventListener("mousedown", () => {
+        if (square.id === state.values.hitPosition) {
+          state.values.result++;
+          state.view.score.textContent = state.values.result;
+          state.values.hitPosition = null;
+          playSound("hit");
+        }  else {
+            loseLife(); // Perde uma vida ao clicar no lugar errado
+        }
+      });
+    });
+  }
+  
+  function initialize() {
+    addListenerHitBox();
+}
+
+initialize();
